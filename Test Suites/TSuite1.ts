ed599758bf41505344203f00d5e1e482d26c712d
dominique.mereaux <dmereaux@gmail.com>
2023-01-20 17:58:45 +0100

    create testsuite

---
+++ Test Suites/TSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>39ff3c3a-2347-41b2-b9b7-7b1b048d8dc3</testSuiteGuid>
+</TestSuiteEntity>
